dc57d3d0ca43ed56f6adb5de0c57ed0be5c5a2e5
krishna saini <krishnapraanjal10@gmail.com>
2024-04-29 08:51:38 +0530

    basic dropdown nav-bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import styles from './NavBar.module.css';
+
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>Item1</li>
+    <nav className={styles.container}>
+      <ul className={styles.list}>
+        <li>
+          <a href='#'>Product</a>
+          <ul className={styles.dropDownList}>
+            <li>
+              <a href='#'>Product1</a>
+            </li>
+            <li>
+              <a href='#'>Product2</a>
+            </li>
+            <li>
+              <a href='#'>Product3</a>
+            </li>
+            <li>
+              <a href='#'>Product4</a>
+            </li>
+          </ul>
+        </li>
+
         <li>Item2</li>
         <li>Item3</li>
         <li>Item4</li>
