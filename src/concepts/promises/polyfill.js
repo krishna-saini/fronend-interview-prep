9415b4ba5f8f08a7351be66926adb0159c050dbf
krishna saini <krishnapraanjal10@gmail.com>
2024-10-14 06:04:33 +0530

    enhance polyfill of promise

--- src/concepts/promises/polyfill.js
+++ src/concepts/promises/polyfill.js
@@ -23,28 +23,24 @@ if (!window.Promise) {
     #onRejectedCallbacks;
 
     constructor(executor) {
-      const PENDING = 'pending';
-      const FULFILLED = 'fulfilled';
-      const REJECTED = 'rejected';
-
-      this.#state = PENDING;
+      this.#state = 'pending';
       this.#value = null;
       this.#onResolvedCallbacks = []; // Callbacks for resolved state
       this.#onRejectedCallbacks = []; // Callbacks for rejected state
 
       const resolve = (result) => {
-        if (this.#state !== PENDING) return;
+        if (this.#state !== 'pending') return;
 
-        this.#state = FULFILLED;
+        this.#state = 'fulfilled';
         this.#value = result;
 
         this.#onResolvedCallbacks.forEach((cb) => cb(this.#value));
       };
 
       const reject = (error) => {
-        if (this.#state !== PENDING) return;
+        if (this.#state !== 'pending') return;
 
-        this.#state = REJECTED;
+        this.#state = 'rejected';
         this.#value = error;
 
         this.#onRejectedCallbacks.forEach((cb) => cb(this.#value));
@@ -106,6 +102,38 @@ if (!window.Promise) {
      */
     catch(rejectedCallback) {
       return this.then(null, rejectedCallback);
+    }
+
+    /**
+     * The Promise.resolve() static method "resolves" a given value to a Promise.
+     */
+    static resolve(value) {
+      return new CustomPromise((resolve) => {
+        resolve(value);
+      });
+    }
+
+    /**
+     * The Promise.reject() static method returns a
+     * Promise object that is rejected with a given reason.
+     */
+    static reject(error) {
+      return new CustomPromise((_, reject) => reject(error));
+    }
+
+    /**
+     * Promise.all -
+     * takes an iterable of promises as input 
+     *  returns a single Promise
+     * 
+     * This returned  promise resolves when all promises in the input array resolve
+     *  (even if the array is empty), providing an array of their resolved values. 
+     * It rejects immediately upon the first promise rejection, passing 
+     * along the reason for that rejection
+     */
+
+    static all(arrOfPromises){
+        return new CustomPromise()
     }
   }
 }
