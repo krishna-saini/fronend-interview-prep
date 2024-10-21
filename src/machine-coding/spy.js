25110947171205523fa3cd5a21b4e6d62f9728b5
krishna-saini <krishnapraanjal10@gmail.com>
2024-10-21 08:44:52 +0530

    implement spy on arguments

--- src/machine-coding/spy.js
+++ src/machine-coding/spy.js
@@ -29,4 +29,17 @@ function spy(func) {
     }
     innerFn.calls = calls;
     return innerFn;
-  }
+}
+
+function work(a, b) {
+  alert( a + b ); // work is an arbitrary function or method
+}
+
+work = spy(work);
+
+work(1, 2); // 3
+work(4, 5); // 9
+
+for (let args of work.calls) {
+  alert( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+}
