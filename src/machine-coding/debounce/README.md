87b9e1d931f2464eae003952c16d8ca5237e4bd7
krishna-saini <krishnapraanjal10@gmail.com>
2024-10-23 07:34:41 +0530

    debounce with trailing and leading flags

--- src/machine-coding/debounce/README.md
+++ src/machine-coding/debounce/README.md
@@ -3,8 +3,21 @@
 
 2. Debouncing is very similar to throttling. The key difference is that throttling enforces limits on continuous operations, while debouncing waits for invocations to stop for a specific time to consolidate many noisy invocations into one single invocation.
 
+https://leetcode.com/problems/debounce/
+https://www.greatfrontend.com/questions/javascript/debounce
+
 
 ## Requirements 
 1. he debounce function returns a new function that takes any number of arguments (...args).
 2. wait time starts from last invocation it means if mulitple invocation happening, start timer after last invocation
 3. Once the timer has elapsed without the debounced function being called again, the function is executed
+
+
+## Extension
+1. In this problem, you are asked to implement an enhanced debounce() which accepts third parameter, option: {leading: boolean, trailing: boolean}
+
+default -> leading false, trailing true
+
+leading: whether to invoke right away
+trailing: whether to invoke after the delay.
+https://bigfrontend.dev/problem/implement-debounce-with-leading-and-trailing-option
--- src/machine-coding/debounce/index.js
+++ src/machine-coding/debounce/index.js
@@ -10,4 +10,48 @@ function debounce(func, wait) {
         func.apply(this, args); // Execute the function after the wait time has passed
       }, wait);
     };
-  }
+}
+
+
+/**
+ * @param {(...args: any[]) => any} func
+ * @param {number} wait
+ * @param {boolean} option.leading
+ * @param {boolean} option.trailing
+ * @returns {(...args: any[]) => any}
+ */
+function debounceExtension1(func, wait, option = { leading: false, trailing: true }) {
+  let timer;
+  let isLeadingCalled = false;
+  
+  return function (...args) {
+    const context = this;
+
+    // Ensure at least one of leading or trailing is enabled
+    if (!option.leading && !option.trailing) {
+      return;
+    }
+
+    // Clear any existing timer to reset the debounce period
+    clearTimeout(timer);
+
+    // Handle the leading option: invoke the function immediately on the first call
+    if (option.leading && !isLeadingCalled) {
+      isLeadingCalled = true;
+      func.apply(context, args);
+      return; // to make sure the leadinf fn does not called again after wait time
+
+    }
+
+    // Set a timer for the trailing call, if needed
+    timer = setTimeout(() => {
+      // Handle trailing option: invoke the function at the end of the debounce period
+      if (option.trailing) {
+        func.apply(context, args);
+      }
+
+      // Reset the leading invocation flag so that to call any function immediately if invoked after wait time is over
+      isLeadingCalled = false;
+    }, wait);
+  };
+}
